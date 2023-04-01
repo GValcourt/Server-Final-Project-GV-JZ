c088f8ba5552404e226e73eade1d014b5d016074
GValcourt <gregory.valcourt@gmail.com>
2023-03-31 20:30:39 -0400

    added functionality to store lat and long
    Required to effectively render the google map

--- controllers/articles/article-controller.js
+++ controllers/articles/article-controller.js
@@ -9,22 +9,46 @@ const ArticleController = (app) => {
     app.put('/api/articles/:uid', updateArticle);
 }
 
+//Converts a location string into a formatted url in order to collect the lat and long for storage
+async function getLocationFromURL (location = "Museum of Contemporary Art Australia") {
+  let url = location.replace(' ', '%20');
+  let response = await fetch(`https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=${url}&inputtype=textquery&fields=formatted_address%2Cname%2Cgeometry&key=AIzaSyBq6A5uqteMK_iK8T-d8YlMFmCw3CyQCWA`);
 
-const createArticle = (req, res) => {
+  if (response.ok) {
+    let jsonObj = await response.json();
+
+    //console.log(jsonObj.candidates[0])
+    return jsonObj.candidates[0]
+  } else {
+    alert("HTTP-Error: " + response.status);
+  }
+}
+
+//console.log(await getLocationFromURL()) //for testing
+
+const createArticle = async (req, res) => {
     const newArticle = req.body;
     newArticle._id = parseInt((new Date()).getTime() + '');
+
+    const value = await getLocationFromURL(newArticle.location.locationName)
+
+    newArticle.location = {
+      ...newArticle.location.locationName,
+      lat : value.location.lat,
+      long : value.location.long
+    }
     articles.push(newArticle);
     res.json(newArticle);
 }
 
-const deleteArticle = (req, res) => {
+const deleteArticle = async (req, res) => {
     const articleId = req.params['uid'];
     articles = articles.filter(art =>
       art._id !== articleId);
     res.sendStatus(200);
 }
 
-const updateArticle = (req, res) => {
+const updateArticle = async (req, res) => {
     const articleId = req.params['uid'];
     const updates = req.body;
     articles = articles.map((art) =>
@@ -35,7 +59,7 @@ const updateArticle = (req, res) => {
     res.sendStatus(200);
 }
 
-const findArticle = (req, res) => {
+const findArticle = async (req, res) => {
     const type = req.query.type
     if(type) {
       const articleOfType = articles
@@ -46,7 +70,7 @@ const findArticle = (req, res) => {
     res.json(articles)
 }
 
-const findArticleById = (req, res) => {
+const findArticleById = async (req, res) => {
     const articleId = req.params.uid;
     const article = articles
       .find(a => a._id === articleId);

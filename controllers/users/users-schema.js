66f61223d1034b400612f26e938ee4b278cae425
GValcourt <83992659+GValcourt@users.noreply.github.com>
2023-04-19 10:36:52 -0400

    Updated Schema
    also users for testing

--- controllers/users/users-schema.js
+++ controllers/users/users-schema.js
@@ -8,6 +8,13 @@ const schema = mongoose.Schema({
   last_name: String,
   password: String, //If this were a live system, I would use encryption
   likes: [String], //Might make this an ObjectId
-  follows: [{type: mongoose.Schema.Types.ObjectId, Model:'LocationModel'}]
-}, {collection: 'articles', versionKey: false });
+  follows: [{type: mongoose.Schema.Types.ObjectId, Model:'LocationModel'}],
+  avatar: String, //stored in firebase
+  bannerPicture: String, //stored in firebase
+  bio: String, //long
+  website: String,
+  location: String,
+  dateJoined: String, //date?
+  reviewNum: String //would int be better?
+}, {collection: 'articles', versionKey: false });//versionKey=true makes an extra object in the database
 export default schema;

d662ff6cb44a4ef74efb9c124783e60e3c70b40d
GValcourt <gregory.valcourt@gmail.com>
2023-04-20 19:14:22 -0400

    Adding article stuff back in

--- controllers/articles/article-controller.js
+++ controllers/articles/article-controller.js
@@ -1,97 +1,117 @@
-import texts from './articles.js'
-let articles = texts
+import * as articleDao from './article-dao.js'
+import * as locationDao from '../locations/location-dao.js'
 
 const ArticleController = (app) => {
-    app.get('/api/articles', findArticle);
+    app.get('/api/articles', findArticles);
     app.get('/api/articles/:uid', findArticleById);
     app.get('/api/articles/byplace/:uid', findArticleByPlace);
-    app.get('/api/articles/pred/:pred/:value', findArticleByPred);
+    app.get('/api/articles/pred/:pred/:value', findArticlePred);
     app.post('/api/articles', createArticle);
     app.delete('/api/articles/:uid', deleteArticle);
     app.put('/api/articles/:uid', updateArticle);
 }
 
-
-
-//console.log(await getPlaceDetails('ChIJ68aBlEKuEmsRHUA9oME5Zh0')) //for testing
-//console.log(await getLocationFromURL("Museum of Contemporary Art Australia")) //for testing
+async function replaceLocationID(articles){
+  for (let j = 0; j < articles.length; j++){
+    let newArray = [];
+    //console.log(articles[j])
+    for (let i = 0; i < articles[j].location.length; i++){
+      let newLocation = await locationDao.findLocationbyMongoID(articles[j].location[i]);
+      //console.log(newLocation)
+      newArray.push(newLocation[0])
+    }
+    //console.log(newArray)
+    articles[j] = {
+      ...articles[j]._doc,
+      location: newArray};
+    //console.log(articles[j].location)
+  }
+  return articles
+}
 
 
 const createArticle = async (req, res) => {
     const newArticle = req.body;
-    newArticle._postid = (new Date()).getTime() + '';
+    let newArray = []
     newArticle.date = (new Date).toDateString();
+    for (let i = 0; i < newArticle.location.length; i++){
+      let locationQuery = await locationDao.findLocationbyPlaceID(newArticle.location[i].placeID)
+      if (locationQuery.length === 0){
+        await locationDao.createLocation(newArticle.location[i])
+      }
+      let mongoID = await locationDao.findLocationbyPlaceID(newArticle.location[i].placeID)
+      //console.log(mongoID[0])
+      newArray.push(mongoID[0]._id)
+    }
+    const orgArticle = {...newArticle};
+    //console.log(newArray)
+    newArticle.location = newArray
+    //console.log(newArticle.location)
+    //console.log(req.body)
     //handling place_id on client side
     //console.log(newArticle)
-    articles.push(newArticle);
-    res.json(newArticle);
+    let insertedArticle = await articleDao.createArticle(newArticle);
+    orgArticle._id = insertedArticle._id
+    res.json(orgArticle);
 }
 
 const deleteArticle = async (req, res) => {
     const articleId = req.params['uid'];
-    articles = articles.filter(art =>
-      art._postid !== articleId);
+    let value = await articleDao.deleteArticle(articleId)
     res.sendStatus(200);
 }
 
 const updateArticle = async (req, res) => {
     const articleId = req.params['uid'];
     const updates = req.body;
-    articles = articles.map((art) =>
-    art._postid === articleId ?
-        {...art, ...updates} :
-        art
-    );
+    let value = await articleDao.updateArticle(articleId, updates)
     res.sendStatus(200);
 }
 
-const findArticle = async (req, res) => {
-    const type = req.query.type
-    if(type) {
-      const articleOfType = articles
-        .filter(a => a.type === type)
-      res.json(articleOfType)
-      return
-    }
-    res.json(articles)
+const findArticles = async (req, res) => {
+  const articles = await articleDao.findArticles();
+  //console.log(articles)
+  let modifiedArticles = await replaceLocationID(articles)
+  //console.log(modifiedArticles)
+  res.json(modifiedArticles);
 }
 
 const findArticleById = async (req, res) => {
     //console.log(req.params)
     const articleId = req.params.uid;
-    const article = articles
-      .find(a => a._postid === articleId);
-    res.json(article);
+    //console.log(articleId)
+    const article = await articleDao.findArticlebyAid(articleId)
+    //waste not; want not
+    let articlePlaceholder = await replaceLocationID([article])
+    //console.log(articlePlaceholder[0])
+    res.json(articlePlaceholder[0]);
 }
 
 const findArticleByPlace = async (req, res) => {
   const place_id = req.params.uid;
-  //console.log(place_id)
-  function searchHandler (locations, place_id) {
-    let testarray = locations.filter(location => location.placeID === place_id)
-    //console.log(testarray)
-    if (testarray.length > 0){
-      //console.log("place_id in the array")
-      return true
-    }
-    else{
-      return false
-    }
+  console.log(place_id)
+  let mongo_id = await locationDao.findLocationbyPlaceID(place_id)
+  console.log(mongo_id)
+  if (mongo_id === null){
+    res.json([])
+    return
   }
-  const articlesOfType = articles
-    .filter(a => searchHandler(a.location, place_id));
-  res.json(articlesOfType);
+  const articles = await articleDao.findArticlesbyLocation(mongo_id._id.toString())
+  //console.log(articles)
+  let modifiedArticles = await replaceLocationID(articles)
+  if (modifiedArticles.length > 0){
+    //console.log(modifiedArticles[0]._id)
+  }
+  res.json(modifiedArticles);
 }
 
 
-const findArticleByPred = async (req, res) => {
+const findArticlePred = async (req, res) => {
   const pred = req.params.pred;
   const value = req.params.value;
-  //console.log(place_id)
-  const articlesOfType = articles
-    .filter(a => a[pred] === value);
-  res.json(articlesOfType);
+  const articles = await articleDao.findArticlesPred(pred, value)
+  let modifiedArticles = await replaceLocationID(articles)
+  res.json(modifiedArticles);
 }
-
 
 export default ArticleController

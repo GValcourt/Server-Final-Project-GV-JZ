3040a8f6bec5756a9a4b39f98914dd2d6a7ae669
GValcourt <gregory.valcourt@gmail.com>
2023-04-23 17:38:09 -0400

    accidentally dropped poster id from the schema

--- controllers/articles/article-schema.js
+++ controllers/articles/article-schema.js
@@ -7,6 +7,7 @@ const schema = mongoose.Schema({
   date: String,
   images: [String],
   location: [{type: mongoose.Schema.Types.ObjectId, Model:'LocationModel'}],
+  _posterID: {type: mongoose.Schema.Types.ObjectId, Model:'UsersModel'},
   private: Boolean
 }, {collection: 'articles', versionKey: false });
 export default schema;
